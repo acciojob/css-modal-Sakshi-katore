88d0c07a7d341461ce2519c528ba19cdeb26e0df
Sakshi Katore <87645647+Sakshi-katore@users.noreply.github.com>
2026-05-04 18:24:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function openModal() {
+  document.querySelector('.modal').style.display = 'block';
+}
+
+document.querySelector('.close-modal').addEventListener('click', function () {
+  document.querySelector('.modal').style.display = 'none';
+});
